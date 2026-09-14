885f4dcb90658505f8a0aa5f2ed4a9cdb634e87e
bakuani <anna2005298@gmail.com>
2026-09-14 15:50:07 +0300

    lab4 try N.2

--- src/main/java/com/rbdip/bookstore/order/OrderRepository.java
+++ src/main/java/com/rbdip/bookstore/order/OrderRepository.java
@@ -1,12 +1,12 @@
 package com.rbdip.bookstore.order;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.EntityGraph;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-	@Override
-	@EntityGraph(attributePaths = {"items", "items.product"})
-	List<Order> findAll();
+    @Override
+    @EntityGraph(attributePaths = {"customer", "items", "items.product"})
+    List<Order> findAll();
 }
